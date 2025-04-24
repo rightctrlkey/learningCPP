8e8c079379b983c95ce346b09487e518f9bb27be
rightctrlkey <amangupta2585@gmail.com>
2025-04-24 22:16:28 +0530

    double

--- begin.cpp
+++ begin.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 
 int main() {
+
     
-    //This is a comment.
+    double price = 10.99;
+    std::cout << price << "\n";
 
-    /*This
-    is a
-    multiline
-    comment*/
 
-    int x; //Declaration
-    x = 5; //Assignment
 
-    std::cout << "I love pizza!" << "\n";
-    std::cout << "I also love dosa!" << "\n";
+
+
     return 0;
 }
