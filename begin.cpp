1e7ca36560039e1d5ab3ecd9b4f14bec2319ce1c
rightctrlkey <amangupta2585@gmail.com>
2025-04-24 22:09:48 +0530

    variable dec and ass

--- begin.cpp
+++ begin.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
+    
+    //This is a comment.
+
+    /*This
+    is a
+    multiline
+    comment*/
+
+    int x;
+    x = 5;
+
     std::cout << "I love pizza!" << "\n";
     std::cout << "I also love dosa!" << "\n";
     return 0;
