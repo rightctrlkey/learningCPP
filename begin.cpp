29798445e2df6ba32bd2a935c157b62c68310fb6
rightctrlkey <amangupta2585@gmail.com>
2025-04-26 00:47:57 +0530

    ternary operator(? : :)

--- begin.cpp
+++ begin.cpp
@@ -5,37 +5,11 @@ using namespace std;
 
 int main() {
 
-    char op;
-    double a;
-    double b;
-    double result;
-
-    cout << "*************CALCULATOR*************\n";
-
-
-    cout << "Enter either (+ - * /)";
-    cin >> op;
-    cout << "Enter A value: ";
+    int a;
+    cout << "enter a value: ";
     cin >> a;
-    cout << "Enter B value: ";
-    cin >> b;
 
-    if(op == '+') {
-        result = a + b;
-        cout << a << op << b << "=" << result << endl;
-    } else if(op == '-') {
-        result = a-b;
-        cout << a << op << b << "=" << result << endl;
-    } else if(op == '*') {
-        result = a * b;
-        cout << a << op << b << "=" << result << endl;
-    } else if(op == '/') {
-        result = a / b;
-        cout << a << op << b << "=" << result << endl;
-    } else {
-        cout << "That wasn't a valid response.";
-    }
+    a > 0 ? cout << "number is positive!" : cout << "Number is negative!";
     
-    
-    cout << "************************************";
+    return 0;
 }
