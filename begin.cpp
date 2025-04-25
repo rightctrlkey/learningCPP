a19e85667d84d7ded2f4782f48be9e4ee4e2523f
rightctrlkey <amangupta2585@gmail.com>
2025-04-26 00:39:35 +0530

    simple calculator

--- begin.cpp
+++ begin.cpp
@@ -5,49 +5,38 @@ using namespace std;
 
 int main() {
 
-    int month;
-    cout << "Enter month (1-12): ";
-    cin >> month;
+    char op;
+    double a;
+    double b;
+    double result;
 
-    switch(month) {
-        case 1:
-            cout << "It is January.";
-            break;
-        case 2:
-            cout << "It is February.";
-            break;
-        case 3:
-            cout << "It is March.";
-            break;
-        case 4:
-            cout << "It is April.";
-            break;
-        case 5:
-            cout << "It is May.";
-            break;
-        case 6:
-            cout << "It is June.";
-            break;
-        case 7:
-            cout << "It is July.";
-            break;
-        case 8:
-            cout << "It is August.";
-            break;
-        case 9:
-            cout << "It is September.";
-            break;
-        case 10:
-            cout << "It is October.";
-            break;
-        case 11:
-            cout << "It is November.";
-            break;
-        case 12:
-            cout << "It is December.";
-            break;
-        default:
-            cout << "Prease enter only in numbers (1-12).";
+    cout << "*************CALCULATOR*************\n";
+
+
+    cout << "Enter either (+ - * /)";
+    cin >> op;
+    cout << "Enter A value: ";
+    cin >> a;
+    cout << "Enter B value: ";
+    cin >> b;
+
+    if(op == '+') {
+        result = a + b;
+        cout << a << op << b << "=" << result << endl;
+    } else if(op == '-') {
+        result = a-b;
+        cout << a << op << b << "=" << result << endl;
+    } else if(op == '*') {
+        result = a * b;
+        cout << a << op << b << "=" << result << endl;
+    } else if(op == '/') {
+        result = a / b;
+        cout << a << op << b << "=" << result << endl;
+
     }
-    return 0;
+
+
+
+
+    cout << "************************************";
 }
