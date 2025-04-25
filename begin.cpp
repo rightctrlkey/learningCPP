b42f0282130f3d45695651eecf8375360e66f5b8
rightctrlkey <amangupta2585@gmail.com>
2025-04-26 00:27:12 +0530

    switch cases

--- begin.cpp
+++ begin.cpp
@@ -5,24 +5,47 @@ using namespace std;
 
 int main() {
 
-    int age;
+    int month;
+    cout << "Enter month (1-12): ";
+    cin >> month;
 
-    cout << "Enter your age: ";
-    cin >> age;
-
-    if(age >= 100) {
-        cout << "You are too old to vote!";
+    switch(month) {
+        case 1:
+            cout << "It is January.";
+            break;
+        case 2:
+            cout << "It is February.";
+            break;
+        case 3:
+            cout << "It is March.";
+            break;
+        case 4:
+            cout << "It is April.";
+            break;
+        case 5:
+            cout << "It is May.";
+            break;
+        case 6:
+            cout << "It is June.";
+            break;
+        case 7:
+            cout << "It is July.";
+            break;
+        case 8:
+            cout << "It is August.";
+            break;
+        case 9:
+            cout << "It is September.";
+            break;
+        case 10:
+            cout << "It is October.";
+            break;
+        case 11:
+            cout << "It is November.";
+            break;
+        case 12:
+            cout << "It is December.";
+            break;
     }
-    else if (age >= 18) {
-        cout << "You can vote!";
-    } 
-    else if(age < 0) {
-        cout << "You aren't born yet!";
-    }
-    
-    else {
-        cout << "You cannot vote!";
-    }
-
     return 0;
 }
