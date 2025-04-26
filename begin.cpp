e48bf5afec1c00e024cb2737a95b423dba108721
rightctrlkey <amangupta2585@gmail.com>
2025-04-26 23:54:13 +0530

    string methods(length)

--- begin.cpp
+++ begin.cpp
@@ -5,30 +5,15 @@ using namespace std;
 
 int main() {
 
-    double temp;
-    char unit;
-
-    cout << "*******temperature conversion********\n";
-
-    cout << "C = Celcius\n";
-    cout << "F = Farenheit\n";
-    cout << "Enter the unit you want the temperature converted to: ";
-    cin >> unit;
-    if (unit == 'C' || unit == 'c') {
-        cout << "Enter the temp in Fahrenheit: ";
-        cin >> temp;
-        cout << (temp - 32) / 1.8 << "C"<< "\n";
-    } else if (unit == 'F' || unit == 'f') {
-        cout << "Enter the temp in Celcius: ";
-        cin >> temp;
-        cout << (1.8 * temp) + 32 << "F" << "\n";
-    } else {
-        cout << "Please enter in only C or F.";
-    }
-
-    cout << "*************************************";
-
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
     
+    if (name.length() > 12) {
+        cout << "Your name cant be more than 12 characters!";
+    } else {
+        cout << "Welcome!!!";
+    }
 
     return 0;
 }
