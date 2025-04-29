4afe625dc24c51dc3097ad8b20c8fc51a3ec4d25
rightctrlkey <amangupta2585@gmail.com>
2025-04-29 20:02:43 +0530

    random event generator

--- index.cpp
+++ index.cpp
@@ -6,14 +6,27 @@ using namespace std;
 
 int main() {
 
-    srand(time(NULL));
+    srand(time(0));
+    int randNum = (rand() % 5) + 1;
 
-    int num1 = (rand() % 6) + 1;
-    int num2 = (rand() % 6) + 1;
-    int num3 = (rand() % 6) + 1;
-    cout << num1 << '\n';
-    cout << num2 << '\n';
-    cout << num3 << '\n';
-    
+    switch(randNum) {
+        case 1: 
+            cout << "You got a 1!";
+            break;
+        case 2: 
+            cout << "You got a 2!";
+            break;
+        case 3: 
+            cout << "You got a 3!";
+            break;
+        case 4: 
+            cout << "You got a 4!";
+            break;
+        case 5: 
+            cout << "You got a 5!";
+            break;
+        default: 
+            cout << "Please enter numbers between 1-5";
+    }
     return 0;
 }
