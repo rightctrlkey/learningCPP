fe9c91397b538e64c1deac8eb1f1ba5a0759419a
rightctrlkey <amangupta2585@gmail.com>
2025-04-29 18:05:59 +0530

    nested for loop

--- index.cpp
+++ index.cpp
@@ -5,8 +5,15 @@ using namespace std;
 
 int main() {
 
-    for (int i = 1; i <= 10; i++){
-        cout << i*4 << endl;
+    for (int i = 1; i <=3; i++) {
+        for(int j = 1; j <=10; j++) {
+        cout << j << ' ';
+        }
+        cout << '\n';
     }
+    
+    
+    
+    
     return 0;
 }
