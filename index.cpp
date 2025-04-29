7890dfe472b4ad94e80f67ddb879f2bdadf135c3
rightctrlkey <amangupta2585@gmail.com>
2025-04-29 20:34:47 +0530

    user defined function

--- index.cpp
+++ index.cpp
@@ -4,29 +4,14 @@
 using namespace std;
 
 
+void HappyBirthday(){
+    cout << "HAPPY BIRTHDAY TO YOU!!\n";
+    cout << "HAPPY BIRTHDAY TO YOU!!\n";
+    cout << "HAPPY BIRTHDAY TO YOU!!\n";
+}
+
 int main() {
 
-    int num;
-    int guess;
-    int tries = 0;
-    srand(time(0));
-    num = rand() % 100 + 1;
-    
-    while(guess != num){
-        cout << "Enter your guess: ";
-        cin >> guess;
-        if (guess > num) {
-            cout << "LOWER!\n";
-            tries++;
-        }
-        else if (guess < num) {
-            cout << "HIGHER! \n";
-            tries++;
-        }
-        else {
-            cout << "GOOD GUESS!! You guessed the number " << num << " in " << tries + 1 << " tries";
-        }
-    }
-
+    HappyBirthday();
     return 0;
 }
